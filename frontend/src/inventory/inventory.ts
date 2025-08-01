82e735e2c204060425d59e0a480f574a45462d8b
dylanee300 <dylanee300@gmail.com>
2025-08-01 16:23:04 -0700

    fix on inv

--- frontend/src/inventory/inventory.ts
+++ frontend/src/inventory/inventory.ts
@@ -1,6 +1,6 @@
 import { doggie } from "./clicker/click";
 
-const totalDoggies = 3; // curent amount of doggies in game, the ones in te current market
+const totalDoggies:number = 3; // curent amount of doggies in game, the ones in te current market
 
 const ownedDoggies = document.querySelectorAll("owned-doggies") as NodeListOf<HTMLDivElement>;
 const totalOwned = document.getElementById("total-owned") as HTMLDivElement;
